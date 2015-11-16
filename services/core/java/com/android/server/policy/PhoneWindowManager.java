2bd1e20d248c0e705831c75854251027eb240a3a
Keith Mok <kmok@cyngn.com>
2015-11-16 13:48:36 -0800

    DeviceKeyHandler: Fix suspend gesture not working
    A function for handling DeviceKey is mis-placed in
    PhoneWindowManager.java when merging from cm-12.1
    
    Change-Id: I5a5421c88216b70defaac69b732d99d506e1a2a6

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -6152,6 +6152,18 @@ private boolean isHwKeysDisabled() {
                 && event.getRepeatCount() == 0
                 && !isHwKeysDisabled();
 
+        // Specific device key handling
+        if (mDeviceKeyHandler != null) {
+            try {
+                // The device only should consume known keys.
+                if (mDeviceKeyHandler.handleKeyEvent(event)) {
+                    return 0;
+                }
+            } catch (Exception e) {
+                Slog.w(TAG, "Could not dispatch event to device key handler", e);
+            }
+        }
+
         // Handle special keys.
         switch (keyCode) {
             case KeyEvent.KEYCODE_BACK: {
